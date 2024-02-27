bca94cfc143a53931ac76bdf31a33c5e475bfab6
Arunkj203 <arunkj203@gmail.com>
2024-02-27 19:19:58 +0530

    basic one

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Items from "./Components/Items";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UploadItem from "./Components/UploadItem";
 import UpdateItem from "./Components/UpdateItem";
-import Pdfview from "./Components/Pdfview";
+
 
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
           <Route path="/upload" element={<UploadItem />} />
           <Route path="/update" element={<UpdateItem />} />
           <Route path="/items" element={<Items />} />
-          <Route path="/pdf" element={<Pdfview/>} />
+     
          
         </Routes>
       </Router>
